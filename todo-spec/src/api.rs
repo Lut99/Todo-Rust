ac3ab53c959461c1a90c8c28bbac18f790afe104
Lut99 <mullertim99@gmail.com>
2022-03-19 12:29:51 +0100

    Added in first API specification

---
+++ todo-spec/src/api.rs
@@ -0,0 +1,27 @@
+/* API.rs
+ *   by Lut99
+ *
+ * Created:
+ *   19 Mar 2022, 12:11:12
+ * Last edited:
+ *   19 Mar 2022, 12:17:37
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Defines various structs that are used in the API communication between
+ *   client and services.
+**/
+
+use serde::{Serialize, Deserialize};
+
+
+/***** LIBRARY STRUCTS *****/
+/// Defines the JSON for the login struct.
+#[derive(Serialize, Deserialize)]
+pub struct LoginJson {
+    /// The username of the user
+    pub username : String,
+    /// The password of the user
+    pub password : String,
+}
--- todo-spec/src/lib.rs
+++ todo-spec/src/lib.rs
@@ -4,7 +4,7 @@
  * Created:
  *   17 Mar 2022, 18:34:58
  * Last edited:
- *   18 Mar 2022, 16:06:17
+ *   19 Mar 2022, 12:13:14
  * Auto updated?
  *   Yes
  *
@@ -18,3 +18,5 @@ pub mod errors;
 
 /// Defines the credential structs and logic.
 pub mod credentials;
+/// Defines the API structs
+pub mod api;
