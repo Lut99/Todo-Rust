4f1860955e6c33b0bb70a9203d252c84a5b19a2c
Lut99 <mullertim99@gmail.com>
2022-03-19 12:29:38 +0100

    Finished credentials for now

--- todo-spec/src/credentials.rs
+++ todo-spec/src/credentials.rs
@@ -4,7 +4,7 @@
  * Created:
  *   17 Mar 2022, 18:35:32
  * Last edited:
- *   18 Mar 2022, 16:36:48
+ *   19 Mar 2022, 10:41:32
  * Auto updated?
  *   Yes
  *
@@ -13,8 +13,14 @@
  *   client and the server.
 **/
 
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 use argonautica::{Hasher, Verifier};
 use argonautica::input::Password;
+use regex::Regex;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub use crate::errors::CredentialError as Error;
 
@@ -22,10 +28,83 @@ pub use crate::errors::CredentialError as Error;
 /***** UNIT TESTS *****/
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_password_simple() {
+        // First, try to make a valid Password credential
+        let cred = match Credential::from_password("john", "this_is_secret!") {
+            Ok(cred) => cred,
+            Err(err) => { panic!("from_password() should not have crashed, but it returned: {}", err); }
+        };
+        #[allow(irrefutable_let_patterns)]
+        if let Credential::Password(user, pass) = cred {
+            assert_eq!(user, String::from("john"));
+            // Check if any hashing has occurred (due to salts we can't check which)
+            assert_ne!(pass, String::from("this_is_a_secret!"));
+        } else {
+            panic!("from_password() returned a non-password {} credential", cred.typename());
+        }
+    }
+
+    #[test]
+    fn test_password_verify() {
+        // Try to make a valid Password credential
+        let cred = match Credential::from_password("john", "this_is_secret!") {
+            Ok(cred) => cred,
+            Err(err) => { panic!("from_password() should not have crashed, but it returned: {}", err); },
+        };
+
+        // Now match it with a matching username/password
+        match cred.verify_password("john", "this_is_secret!") {
+            Ok(res)  => { assert_eq!(res, true); },
+            Err(err) => { panic!("verify_password() should not have crashed, but it returned: {}", err); },
+        }
+        // Now match it with a not-matching username
+        match cred.verify_password("john2", "this_is_secret!") {
+            Ok(res)  => { assert_eq!(res, false); },
+            Err(err) => { panic!("verify_password() should not have crashed, but it returned: {}", err); },
+        }
+        // Now match it with a not-matching password
+        match cred.verify_password("john", "this_is_secret?") {
+            Ok(res)  => { assert_eq!(res, false); },
+            Err(err) => { panic!("verify_password() should not have crashed, but it returned: {}", err); },
+        }
+    }
+
+    #[test]
+    fn test_password_salt() {
+        // Make two hashes of the same password
+        let cred1 = match Credential::from_password("john", "this_is_secret!") {
+            Ok(cred) => cred,
+            Err(err) => { panic!("from_password() should not have crashed, but it returned: {}", err); }
+        };
+        let cred2 = match Credential::from_password("john", "this_is_secret!") {
+            Ok(cred) => cred,
+            Err(err) => { panic!("from_password() should not have crashed, but it returned: {}", err); }
+        };
+
+        // Make sure that the hashes are _not_ the same
+        #[allow(irrefutable_let_patterns)]
+        if let (Credential::Password(_, pass1), Credential::Password(_, pass2)) = (&cred1, &cred2) {
+            assert_ne!(pass1, pass2);
+        } else {
+            panic!("from_password() returned non-password ({}, {}) credential(s)", cred1.typename(), cred2.typename());
+        }
+    }
+
+    #[test]
+    fn test_illegal_username() {
+        // Make a hash with a username that is illegal
+        let _ = match Credential::from_password("john#$", "this_is_secret!") {
+            Ok(_)    => { panic!("from_password() should have crashed due to illegal username, but it didn't"); },
+            Err(err) => {
+                match err {
+                    Error::InvalidUsername{ username: _ } => {},
+                    err => { panic!("from_password() crashed, but not because of an illegal username: {}", err); }
+                }
+            },
+        };
     }
 }
 
@@ -33,26 +112,43 @@ mod tests {
 
 
 
+/***** CONSTANTS *****/
+/// Defines the regular expression that is used to match usernames.
+const USERNAME_REGEX: &str = r"^[0-9a-zA-Z_-]+$";
+
+
+
+
+
 /***** LIBRARY STRUCTS *****/
 /// Defines one of multiple types of Credentials.
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum Credential {
     /// The user has given us an (already hashed) password.
-    Password(String),
+    Password(String, String),
 }
 
 impl Credential {
     /// Constructor for the Credential that takes a plain-text password and hashes it.
     /// 
     /// **Generic types**
-    ///  * `P`: The type of the Password that is passed to the function.
+    ///  * `S`: The String-like type of the username that is passed to the function.
+    ///  * `P`: The type of the password that is passed to the function.
     /// 
     /// **Arguments**
+    ///  * `username`: The username of the user to whom the given password belongs.
     ///  * `password`: The plain text password we would like to hash as fast as possible.
     /// 
     /// **Returns**  
     /// The new Credential instance on success, or else an Error.
-    pub fn from_password<'a, P: Into<Password<'a>>>(password: P) -> Result<Self, Error> {
+    pub fn from_password<'a, S: Into<String>, P: Into<Password<'a>>>(username: S, password: P) -> Result<Self, Error> {
+        // Convert String-like into String
+        let username = username.into();
+
+        // Verify that the username contains no illegal characters
+        let re = Regex::new(USERNAME_REGEX).expect("Illegal Regex for matching usernames; this should never happen!");
+        if !re.is_match(&username) { return Err(Error::InvalidUsername{ username }); }
+
         // Build the hasher
         let mut hasher = Hasher::default();
         let hash = match hasher
@@ -65,29 +161,108 @@ impl Credential {
         };
 
         // Create a new Credential with this hash
-        Ok(Credential::Password(hash))
+        Ok(Credential::Password(username, hash))
     }
 
-    
-
-    /// Compares this Credential to the given password.
+    /// Constructor for the Credential that loads a username/hashed password pair from disk.
     /// 
     /// **Generic types**
-    ///  * `P`: The type of the Password that is passed to the function.
+    ///  * `P`: The Path-like type of the path that is passed to the function.
     /// 
-    /// **Arguments**  
-    ///  * `password`: The plain text password we would like to verify.
+    /// **Arguments**
+    ///  * `path`: A path-like that refers to the file where the username and hashed password is stored.
+    /// 
+    /// **Returns**  
+    /// The new Credential instance on success, or else an Error.
+    pub fn from_password_file<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
+        // Convert Path-like into Path
+        let path: &Path = path.as_ref();
+
+        // Try to open the file
+        let mut handle = match File::open(path) {
+            Ok(handle) => handle,
+            Err(err)   => { return Err(Error::FileOpenError{ path: path.to_path_buf(), err }); }
+        };
+
+        // Try to read everything in the file
+        let mut data = String::new();
+        if let Err(err) = handle.read_to_string(&mut data) {
+            return Err(Error::FileReadError{ path: path.to_path_buf(), err });
+        }
+
+        // Use the 'from_string' constructor to split the string and deal with it
+        drop(handle);
+        Self::from_password_string(data)
+    }
+
+    /// Constructor for the Credential that takes a given username/password string and parses it.
+    /// 
+    /// **Generic types**
+    ///  * `S`: The String-like type of the serialized username/password pair that is passed to the function.
+    /// 
+    /// **Arguments**
+    ///  * `string`: A String-like that constains the serialized username/password pair we will base a new Credential on.
+    /// 
+    /// **Returns**  
+    /// The new Credential instance on success, or else an Error.
+    pub fn from_password_string<S: Into<String>>(string: S) -> Result<Self, Error> {
+        // Convert the String-like into a String
+        let string: String = string.into();
+
+        // Try to split the string on the first '+' we see
+        let mut found = false;
+        let mut username = String::with_capacity(string.len());
+        let mut password = String::with_capacity(string.len());
+        for c in string.graphemes(true) {
+            if !found && c == "+" {
+                found = true;
+            } else if !found {
+                username.push_str(c);
+            } else {
+                password.push_str(c);
+            }
+        }
+        // Check we found one
+        if !found { return Err(Error::MissingSeparator); }
+
+        // Make sure the username is legal
+        let re = Regex::new(USERNAME_REGEX).expect("Illegal Regex for matching usernames; this should never happen!");
+        if !re.is_match(&username) { return Err(Error::InvalidUsername{ username }); }
+
+        // That's it! Now return it
+        username.shrink_to_fit();
+        password.shrink_to_fit();
+        Ok(Credential::Password(username, password))
+    }
+
+
+
+    /// Compares this Credential to the given username + password.
+    /// 
+    /// **Generic types**
+    ///  * `S`: The String-like type of the username that is passed to the function.
+    ///  * `P`: The type of the password that is passed to the function.
+    /// 
+    /// **Arguments**
+    ///  * `username`: The username of the user to whom the given password belongs.
+    ///  * `password`: The plain text password we would like to hash as fast as possible.
     /// 
     /// **Returns**  
     /// Whether or not the passwords match, or else an Error if some error occurred while hashing.
-    pub fn verify_password<'a, P: Into<Password<'a>>>(&self, password: P) -> Result<bool, Error> {
+    pub fn verify_password<'a, S: Into<String>, P: Into<Password<'a>>>(&self, username: S, password: P) -> Result<bool, Error> {
+        // Convert String-like into String
+        let username = username.into();
+
         // Make sure we are a Password credential
         #[allow(irrefutable_let_patterns)]
-        if let Credential::Password(password_hash) = self {
-             // Build the verifier
+        if let Credential::Password(self_username, self_password) = self {
+            // First, make sure the username makes sense
+            if self_username != &username { return Ok(false); }
+
+            // Build the verifier
             let mut verifier = Verifier::default();
             let is_valid = match verifier
-                .with_hash(password_hash)
+                .with_hash(self_password)
                 .with_password(password)
                 .verify()
             {
@@ -108,7 +283,7 @@ impl Credential {
     #[inline]
     pub fn typename(&self) -> &'static str {
         match self {
-            Credential::Password(_) => "Password",
+            Credential::Password(_, _) => "Password",
         }
     }
 }
--- todo-spec/src/errors.rs
+++ todo-spec/src/errors.rs
@@ -4,7 +4,7 @@
  * Created:
  *   18 Mar 2022, 16:04:08
  * Last edited:
- *   18 Mar 2022, 16:35:35
+ *   19 Mar 2022, 09:43:18
  * Auto updated?
  *   Yes
  *
@@ -14,14 +14,25 @@
 
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FResult};
+use std::path::PathBuf;
 
 
 /***** ERRORS *****/
 /// Errors that occur while working with credentials.
 #[derive(Debug)]
 pub enum CredentialError {
+    /// The given username was not valid.
+    InvalidUsername{ username: String },
     /// Could not hash the given password.
     PasswordHashError{ err: argonautica::Error },
+
+    /// Could not open the given file
+    FileOpenError{ path: PathBuf, err: std::io::Error },
+    /// Could not read from the given file
+    FileReadError{ path: PathBuf, err: std::io::Error },
+
+    /// Didn't find the split '+' in the username/character pair.
+    MissingSeparator,
 
     /// The given credential is not valid to check as a password
     VerifyPasswordWrongCredential{ got: &'static str },
@@ -30,7 +41,13 @@ pub enum CredentialError {
 impl Display for CredentialError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FResult {
         match self {
-            CredentialError::PasswordHashError{ err } => write!(f, "Could not hash password: {}", err),
+            CredentialError::InvalidUsername{ username } => write!(f, "Given username '{}' is invalid; it may only contain alphanumerical characters and underscores (_) and dashes(-)", username),
+            CredentialError::PasswordHashError{ err }    => write!(f, "Could not hash password: {}", err),
+
+            CredentialError::FileOpenError{ path, err } => write!(f, "Could not open file '{}': {}", path.display(), err),
+            CredentialError::FileReadError{ path, err } => write!(f, "Could not read from file '{}': {}", path.display(), err),
+
+            CredentialError::MissingSeparator => write!(f, "Missing username/password separator '+' in serialized username/password pair"),
 
             CredentialError::VerifyPasswordWrongCredential{ got } => write!(f, "Cannot verify credential of type {} as a password", got),
         }
